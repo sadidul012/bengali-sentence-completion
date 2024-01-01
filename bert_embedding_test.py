725abacade5d0c03d292fe7c7e5e76a2ac0edee2
Sadidul I <sadidul.012@gmail.com>
2024-01-01 15:35:24 +0600

    Test complete

--- bert_embedding_test.py
+++ bert_embedding_test.py
@@ -9,7 +9,8 @@ def mean_pooling(model_output, attention_mask):
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-    return sum_embeddings / sum_mask
+    # return sum_embeddings / sum_mask
+    return token_embeddings
 
 
 tokenizer = AutoTokenizer.from_pretrained("./bert-tokenizer-bangla")
@@ -17,7 +18,9 @@ model = AutoModelForMaskedLM.from_pretrained("./bert-base-uncased-bangla")
 encoded_input = tokenizer("বৃক্ষ পত্রহীন নয়", return_tensors='pt')
 model_output = model(**encoded_input)
 embedding = mean_pooling(model_output, encoded_input['attention_mask'])
-closest = torch.topk(embedding.flatten(), 30).indices
+closest = embedding.argmax(-1).squeeze(0).numpy()
+# print(closest)
+# # closest = torch.topk(embedding.flatten(), 30).indices
 c = 0
 k = 5
 next_words = []
